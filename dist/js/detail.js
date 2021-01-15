e0437019fabef2d3ee3b340085152922423722fb
georit <georit01@gmail.com>
2021-01-15 19:48:26 +0000

    Refined overall functionality

---
+++ dist/js/detail.js
@@ -0,0 +1,123 @@
+/* ******variables****** */
+const btnBack = document.getElementById("btn-back");
+
+/* ******functions****** */
+async function getCountryDetailsByName(name) {
+  const resp = await fetch(`https://restcountries.eu/rest/v2/name/${name}`);
+  const data = await resp.json();
+
+  let countryData = data[0];
+  return countryData;
+}
+
+async function getCountryDetailsByCode(code) {
+  const resp = await fetch(`https://restcountries.eu/rest/v2/alpha/${code}`);
+  const data = await resp.json();
+
+  let countryData = data;
+  return countryData;
+}
+
+function displayCountryDetails(data) {
+  let countryDetails = document.createElement("div");
+  countryDetails.classList.add("country-details");
+
+  countryDetails.innerHTML = `
+  <div class="country-details-container">
+  <button class="btn-back" id="btn-back">
+	<i class="far fa-long-arrow-left"></i> Back
+  </button>
+  <div class="country-flag-container" style="background: url('${
+    data.flag
+  }') 0 0 / cover
+	no-repeat;"></div>
+  <div class="country-details">
+	<p class="country-name">${data.name}</p>
+	<div class="country-main-details">
+	  <p class="country-detail">
+		<span class="detail-title">Native Name:</span> ${data.nativeName}
+	  </p>
+	  <p class="country-detail population">
+		<span class="detail-title">Population:</span> ${data.population.toLocaleString()}
+	  </p>
+	  <p class="country-detail">
+		<span class="detail-title">Region:</span> ${data.region}
+	  </p>
+	  <p class="country-detail">
+		<span class="detail-title">Sub Region:</span> ${data.subregion}
+	  </p>
+	  <p class="country-detail">
+		<span class="detail-title">Capital:</span> ${data.capital}
+	  </p>
+	</div>
+	<div class="country-other-details">
+	  <p class="country-detail">
+		<span class="detail-title">Top Level Domain:</span> ${data.topLevelDomain}
+	  </p>
+	  <p class="country-detail">
+		<span class="detail-title">Currencies:</span>  ${getCurrencies(data).join(", ")}
+	  </p>
+	  <p class="country-detail">
+		<span class="detail-title">Languages:</span> ${getLanguages(data).join(", ")}
+	  </p>
+	</div>
+	<div class="border-countries-container">
+	  <p class="border-countries-heading">Border Countries</p>
+	  <div class="border-countries" id="border-countries"></div>
+	</div>
+  </div>
+</div>
+  `;
+
+  document.querySelector(".wrapper").append(countryDetails);
+
+  // border countries
+  data.borders.forEach(async function (border) {
+    let details = await getCountryDetailsByCode(border);
+
+    let button = document.createElement("button");
+    button.classList.add("btn-border-country");
+    button.innerHTML = details.name;
+    document.querySelector(".border-countries").appendChild(button);
+  });
+
+  /* ******listen for button clicks****** */
+  // border country button
+  document.getElementById("border-countries").addEventListener("click", (e) => {
+    if (e.target.className === "btn-border-country") {
+      // clear country details
+      countryDetails.remove();
+      // display new country data
+      getCountryDetailsByName(e.target.innerText).then((countryData) =>
+        displayCountryDetails(countryData)
+      );
+    }
+  });
+
+  // back button
+  document.getElementById("btn-back").addEventListener("click", () => {
+    // destroy countries element
+    countryDetails.remove();
+    // display home
+    document.getElementById("search-filter-countries-container").style.display =
+      "block";
+  });
+}
+
+function getCurrencies(data) {
+  let monies = [];
+  data.currencies.forEach((currency) => {
+    monies.push(currency.name);
+  });
+  return monies;
+}
+
+function getLanguages(data) {
+  let lingos = [];
+  data.languages.forEach((currency) => {
+    lingos.push(currency.name);
+  });
+  return lingos;
+}
+
+/* ******event listeners****** */
---
+++ dist/js/main.js
@@ -0,0 +1,171 @@
+/* ******variables****** */
+const themeLink = document.getElementById("theme-link");
+const themeSwitch = document.getElementById("toggle-switch");
+const themeSwitchIcon = document.getElementById("toggle-switch-icon");
+const themeSwitchText = document.getElementById("toggle-switch-text");
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const filterToggle = document.getElementById("filter-instruction");
+const filterToggleIcon = document.getElementById("icon-filter-instruction");
+const filterOptions = document.getElementById("filter-options");
+const countriesEl = document.getElementById("countries");
+
+/* ******on load****** */
+getAllCountriesData();
+
+/* ******functions****** */
+function showFilterOptions() {
+  if (filterOptions.classList.contains("hidden")) {
+    //show filter options
+    filterOptions.classList.remove("hidden");
+    // change toggle icon
+    filterToggleIcon.className = "fas fa-chevron-down icon-filter-instruction";
+  } else {
+    //hide filter options
+    filterOptions.classList.add("hidden");
+    // change toggle icon
+    filterToggleIcon.className = "fas fa-chevron-up icon-filter-instruction";
+  }
+}
+
+async function getAllCountriesData() {
+  try {
+    const resp = await fetch("https://restcountries.eu/rest/v2/all");
+    const data = await resp.json();
+
+    data.forEach((item) => createCountryEL(item));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function searchCountryByName(name) {
+  try {
+    const resp = await fetch(`https://restcountries.eu/rest/v2/name/${name}`);
+    const data = await resp.json();
+
+    if (data) {
+      data.forEach((item) => createCountryEL(item));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getCountriesByRegion(region) {
+  try {
+    const resp = await fetch(
+      `https://restcountries.eu/rest/v2/region/${region}`
+    );
+    const data = await resp.json();
+
+    if (data) {
+      data.forEach((item) => createCountryEL(item));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createCountryEL(info) {
+  let country = document.createElement("div");
+  country.classList.add("country");
+
+  country.innerHTML = `
+  <div class="country-flag-container" style="background: url('${
+    info.flag
+  }') 0 0 / cover no-repeat;"></div>
+  <div class="country-info">
+	<p class="country-name">${info.name}</p>
+	<p class="country-population">
+	  <span class="population-data country-data">Population:</span>
+	  ${info.population.toLocaleString()}
+	</p>
+	<p class="country-region">
+	  <span class="region-data country-data">Region:</span>
+	  ${info.region}
+	</p>
+	<p class="country-capital">
+	  <span class="capital-data country-data">Capital:</span>
+	  ${info.capital}
+	</p>
+  </div>`;
+
+  countriesEl.appendChild(country);
+}
+
+function filterCountries(option) {
+  let countries = [...document.querySelectorAll(".country")];
+
+  countries.forEach((country) => {
+    if (!country.classList.contains(option)) {
+      country.classList.add("hidden");
+    } else {
+      country.classList.remove("hidden");
+    }
+  });
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/* ******event listeners****** */
+// switch theme
+themeSwitch.addEventListener("click", () => {
+  if (themeLink.getAttribute("href") === "./dist/css/styles_light.css") {
+    // change stylesheet
+    themeLink.setAttribute("href", "./dist/css/styles_dark.css");
+    // change switch text
+    themeSwitchText.innerText = "Light Mode";
+  } else {
+    // change stylesheet
+    themeLink.setAttribute("href", "./dist/css/styles_light.css");
+    // change switch text
+    themeSwitchText.innerText = "Dark Mode";
+  }
+});
+
+// show filter options
+filterToggle.addEventListener("click", () => {
+  showFilterOptions();
+});
+
+// filter countries
+filterOptions.addEventListener("click", (e) => {
+  let clickedEl = e.target;
+
+  if (clickedEl.className === "filter-option") {
+    // clean up countries
+    countriesEl.innerHTML = "";
+    // display filtered countries
+    getCountriesByRegion(clickedEl.id);
+    // hide filter options
+    showFilterOptions();
+  }
+});
+
+// search for country
+searchForm.addEventListener("submit", (e) => {
+  if (searchInput.value.length > 0) {
+    // clear countries
+    countriesEl.innerHTML = "";
+    searchCountryByName(searchInput.value);
+  }
+  e.preventDefault();
+});
+
+// event listener to display country details
+countriesEl.addEventListener("click", (e) => {
+  if (e.target.classList.contains("country")) {
+    let countryName = e.target.children[1].firstElementChild.innerText;
+
+    // hide homepage
+    document.getElementById("search-filter-countries-container").style.display =
+      "none";
+
+    getCountryDetailsByName(countryName).then((countryData) =>
+      displayCountryDetails(countryData)
+    );
+  }
+});
